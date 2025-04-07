27f39b51fdc31b23c66e60f7bc3dd269d4f43460
codewithzaqar <zaqarstandoff@gmail.com>
2025-04-07 19:34:54 +0400

    HTTP requests error handling, adding cmdline params

--- whotrack.py
+++ whotrack.py
@@ -1,21 +1,40 @@
 import requests
 import json
 import os
+import sys
 
 def write_to_file(url, fname):
     with open(fname, "a") as f:
         f.write(url+"\n")
 
+def make_request(url, headers, error_type):
+    try:
+        r = requests.get(url, headers=headers)
+        if r.status_code:
+            return r, error_type
+    except requests.exceptions.HTTPError as errh:
+        print ("HTTP Error: ", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting: ", errc)
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error: ", errt)
+    except requests.exceptions.RequestException as err:
+        print ("Unknown error: ", err)
+    return None, ""
 
 def main():
     # Not sure why, but the banner messas up if i put into one print function
-    print("\033[37;1m  __        ___           _                  _      ")
-    print("\033[37;1m  \ \      / / |__   ___ | |_ _ __ __ _  ___| | __  ")
-    print("\033[37;1m   \ \ /\ / /| '_ \ / _ \| __| '__/ _` |/ __| |/ /  ")
-    print("\033[37;1m    \ V  V / | | | | (_) | |_| | | (_| | (__|   <   ")
-    print("\033[37;1m     \_/\_/  |_| |_|\___/ \__|_|  \__,_|\___|_|\_\  ")
+    print("  __        ___           _                  _      ")
+    print("  \ \      / / |__   ___ | |_ _ __ __ _  ___| | __  ")
+    print("   \ \ /\ / /| '_ \ / _ \| __| '__/ _` |/ __| |/ /  ")
+    print("    \ V  V / | | | | (_) | |_| | | (_| | (__|   <   ")
+    print("     \_/\_/  |_| |_|\___/ \__|_|  \__,_|\___|_|\_\  ")
 
-    username = input("\033[92;1m[\033[37;1m?\033[92;1m]\033[92;1m Input Username: \033[0m")
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        username = input("\033[92;1m[\033[37;1m?\033[92;1m]\033[92;1m Input Username: \033[0m")
+    
     print()
 
     fname = username+".txt"
@@ -24,7 +43,6 @@ def main():
         os.remove(fname)
         print("\033[1;92m[\033[0m\033[1;77m*\033[0m\033[1;92m] Removing previous file:\033[1;37m {}\033[0m".format(fname))
 
-    
     print("\033[1;92m[\033[0m\033[1;77m*\033[0m\033[1;92m] Checking username\033[0m\033[1;37m {}\033[0m\033[1;92m on: \033[0m".format(username))
     raw = open("data.json", "r")
     data = json.load(raw)
@@ -40,8 +58,7 @@ def main():
         url = data.get(social_network).get("url").format(username)
         error_type = data.get(social_network).get("errorType")
 
-
-        r = requests.get(url, headers=headers)
+        r, error_type = make_request(url=url, headers=headers, error_type=error_type)
 
         if error_type == "message":
             error = data.get(social_network).get("errorMsg")
@@ -61,6 +78,9 @@ def main():
 
             else:
                 print("\033[37;1m[\033[91;1m-\033[37;1m]\033[92;1m {}:\033[93;1m Not Found!".format(social_network))
+
+        elif error_type == "":
+            print("\033[37;1m[\033[91;1m-\033[37;1m]\033[92;1m{}:\033[93;1m Error!".format(social_network))
             
         elif error_type == "response_url":
             error = data.get(social_network).get("errorMsgInUrl")
